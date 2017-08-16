3f48602473f90ccdfec0f3ad7fda7182cb626b47
mitulknowarth <30891451+mitulknowarth@users.noreply.github.com>
2017-08-16 19:11:56 +0530

    new cordova imports added
    new cordova imports added

--- src/android/FingerprintHelper.java
+++ src/android/FingerprintHelper.java
@@ -26,6 +26,15 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import org.apache.cordova.CordovaInterface;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by mitul.varmora on 01-08-2017.
  */
